c0716b2e12240dd88075be59acb92aac80b42873
antonella <antonella.gr27@gmail.com>
2019-12-12 17:18:29 -0800

    story routes all tested

--- e2e/__tests__/stories.test.js
+++ e2e/__tests__/stories.test.js
@@ -16,6 +16,16 @@ describe('Story API', () => {
     storySynopsis: 'this must be a synopsis test'
   };
 
+  const story2 = {
+    storyTitle: 'two two',
+    storySynopsis: 'uuhhh testing this'
+  };
+
+  const story3 = {
+    storyTitle: 'testy three',
+    storySynopsis: 'test test test'
+  };
+
   async function postStory(posting) {
     return await request
       .post('/api/v1/stories')
@@ -45,5 +55,49 @@ describe('Story API', () => {
       });
   });
 
- 
+  it('gets all stories', () => {
+    return Promise.all([
+      postStory(story),
+      postStory(story2),
+      postStory(story3)
+    ])
+      .then(() => {
+        return request
+          .get('/api/v1/stories')
+          .expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.length).toBe(3);
+        expect(body[0]).toEqual({ '__v': 0, '_id': expect.any(String), 'storyGenre': [], 'storySynopsis': 'this must be a synopsis test', 'storyTags': [], 'storyTitle': 'writing a test', 'userId': '7890', 'userImage': 'k.png', 'userName': 'p' });
+      });
+  });
+
+  it('can edit a story', () => {
+    let storyId;
+    return postStory(story)
+      .then(story => {
+        storyId = story._id;
+        return request
+          .put(`/api/v1/stories/${storyId}`)
+          .send({
+            storyTitle: 'updating title', storyDescription: 'testing' })
+          .expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.storyTitle).toBe('updating title');
+      });
+  });
+
+  it('can delete a story', () => {
+    let storyId;
+    return postStory(story)
+      .then(story => {
+        storyId = story._id;
+      })
+      .then(() => {
+        return request
+          .delete(`/api/v1/stories/${storyId}`)
+          .expect(200);
+      });
+  });
 });
